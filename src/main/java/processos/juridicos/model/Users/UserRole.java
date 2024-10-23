abe6416d560916ff83aff34aed74c980a379a929
Amauri M Ferreira <amauri816@hotmail.com>
2024-10-23 20:12:58 -0300

    criacao enum UserRole

---
+++ src/main/java/processos/juridicos/model/Users/UserRole.java
@@ -0,0 +1,16 @@
+package processos.juridicos.model.Users;
+
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    UserRole(String role){
+        this.role = role;
+    }
+    
+    public String getRole(){
+        return role;
+    }
+}

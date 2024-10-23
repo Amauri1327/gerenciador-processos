8c863ec9fe1f99b1fc3f6169ec7c0b8798f1c70c
Amauri M Ferreira <amauri816@hotmail.com>
2024-10-23 20:15:29 -0300

    implementacao userDetails

--- src/main/java/processos/juridicos/model/Users/User.java
+++ src/main/java/processos/juridicos/model/Users/User.java
@@ -1,6 +1,7 @@
 package processos.juridicos.model.Users;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -16,36 +17,43 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        if (this.role == UserRole.ADMIN)
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+
+        if (this.role == UserRole.USER)
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+
+        else
+            return null;
     }
 
     @Override
     public String getPassword() {
-        return "";
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return login;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return true;
     }
 }

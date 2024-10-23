ec264dfe19191f8ca0d7e6b3d530c6e1f688cdc1
Amauri M Ferreira <amauri816@hotmail.com>
2024-10-23 20:25:19 -0300

    criacao da AuthorizationService

--- src/main/java/processos/juridicos/model/Users/User.java
+++ src/main/java/processos/juridicos/model/Users/User.java
@@ -1,5 +1,10 @@
 package processos.juridicos.model.Users;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,13 +12,26 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
+@Entity
+@Table(name = "tb_users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class User implements UserDetails {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private Long id;
     private String login;
     private String password;
     private UserRole role;
 
+    public User(String login, String password, UserRole role){
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
--- src/main/java/processos/juridicos/repositories/UserRepository.java
+++ src/main/java/processos/juridicos/repositories/UserRepository.java
@@ -2,8 +2,10 @@ package processos.juridicos.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 import processos.juridicos.model.Users.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     UserDetails findByLogin(String login);
 }
